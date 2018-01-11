526af567fbd60689318453ec68904afc9dbe2314
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-11 16:45:55 +0530

    Create intprime.c

---
+++ intprime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+int n, i, flag = 0,;
+printf("Enter a positive integer: ");
+scanf("%d",&n);
+printf("enter the limits");
+scanf("%d%d",&sta,&sto);
+for(j=sta;j<=stp;j++)
+{
+for(i=2; i<=n/2; ++i)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+if (flag==0)
+printf("%d is a prime number.",n);
+else
+printf("%d is not a prime number.",n);
+}
+}
