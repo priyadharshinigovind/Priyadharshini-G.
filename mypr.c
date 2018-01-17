1d91e8ce16780e049a54945c9a42f19af01656db
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-17 22:31:27 +0530

    Create mypr.c

---
+++ mypr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+int a, i,j, flag = 0;
+int start,stop;
+printf("enter number");
+scanf("%d",&a);
+printf("enter the limits");
+scanf("%d%d",&start,&stop);
+for(j=start;j<=stop;j++)
+{
+for(i=2; i<=a/2; ++i)
+{
+if(a%i==0)
+{
+flag=1;
+break;
+}
+}
+if (flag==0)
+printf("it is a prime number");
+else
+printf("it is not a prime number");
+}
+}
