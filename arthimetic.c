daf010da17bac30ac038544bbeac9106103ba7d1
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-18 15:55:41 +0530

    Create arthimetic.c

---
+++ arthimetic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int resu, lim,a,d;
+printf("Enter the limit, start, and difference");
+scanf("%d%d%d",&lim,&a,&d);
+resu=(n/2)*(2*a+(n-1)*d);
+printf("%d",resu);
+}
