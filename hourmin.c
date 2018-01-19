0a15dcb9afd7ac8b5a17fcab7ec32bcc03828884
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-19 16:06:09 +0530

    Update hourmin.c

--- hourmin.c
+++ hourmin.c
@@ -3,5 +3,18 @@ void main()
 {
 int hr,min;
 printf("enter minutes");
-scanf("%d");
+scanf("%d",&min);
+  if(min<60)
+  {
+    printf("0:%d",min);
+  }
+  elseif(min==60)
+  {
+    printf("1:00");
+  }
+  else
+  {
+    hr=min/60;
+    printf("%d",hr);
+  }
 }
