2bf7f5b426f89251c173c04fc593ec0642103b19
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-19 15:51:28 +0530

    Update printindex.c

--- printindex.c
+++ printindex.c
@@ -2,5 +2,13 @@
 #include<conio.h>
 void main()
 {
-int a[],;
+  int a[70],i,lim=6;
+  printf("enter list of elements");
+  for(i=0;i<lim;i++)
+  scanf("%d",&a[i]);
+  for(i=0;i<lim;i++)
+  {
+    printf("%d   %d",a[i],i);
+  }
+  
 }
