2ad01c1ddd77c8bbd6ab0e3620d006effa9c8f01
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-21 11:26:49 +0530

    Create stringcmp.c

---
+++ stringcmp.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char str1[67],str2[234];
+int i;
+printf("Enter two strings");
+scanf("%s%s",&str1,&str2);
+for(i=0;str1[i]!=null;i++)
+{
+if(str1[i]==str2[i])
+{
+temp=1;
+}
+else
+{
+temp=0
+}
+}
+}
