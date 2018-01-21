997bf31f09645a313a4ba813094fee902041a960
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-21 09:42:24 +0530

    Create ghd.c

---
+++ ghd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char sen[50];
+printf("Enter paragraph");
+scanf("%s",&sen);
+int i,count=0;
+for(i=-0;s[i]!=""\n;i++)
+{
+if(s[i]==".")
+{
+count++;
+}
+}
+printf("%d",count);
+}
