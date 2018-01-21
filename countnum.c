993231fbf06d287927c054f2a4c9c61b7c8e9f7a
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-21 09:59:08 +0530

    Create countnum.c

---
+++ countnum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[50];
+int i,count=0;
+printf("Enter the senet");
+scanf("%s",&a);
+for(i=0;a[i]!=null;i++)
+{
+if(a[i]==1-9)
+{
+count++;
+}
+}
+printf("%d",count);
+}
