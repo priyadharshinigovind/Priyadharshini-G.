8ff3ee41b508eacf9ac3823ec73eafff4e740c7e
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-01-21 11:43:19 +0530

    Create concate.c

---
+++ concate.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char r1[20],r2[20];
+int i,j;
+printf("enter two strings");
+scanf("%s%s",&r1,&r2);
+i=0;
+j=0;
+if(r1[i]!="\n")
+i++;
+if(r2[j]!="\n")
+{
+r[i]=r[j];
+i++;
+j++;
+}
+printf("%s",r1);
+}
