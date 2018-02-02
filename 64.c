36c48bedab0a0bdca29fe703ec06e8034a52a17a
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-02-02 09:12:48 +0000

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int m,n;
+printf("\n Enter two numbers");
+scanf("%d%d",&m,&n);
+if((m+n)%2==0)
+{
+  printf("\nThe number is even");
+}
+else
+{
+  printf("\n Odd");
+}
+}
